92faa5438ad8f291b26292fcd5ab8b46b4a80985
felixarydwisaputra <felixarydwisaputra@gmail.com>
2024-07-01 23:17:20 +0800

    Menambahkan Project 3 - dengan Vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import Project1 from './components/Project1/main.vue'
   import Project2 from './components/Project2/main.vue'
+  import Project3 from './components/Project3/main.vue'
 
 </script>
 
@@ -9,5 +10,8 @@
   <!-- <Project1/> -->
 
   <!-- Project 2 -->
-  <Project2/>
+  <!-- <Project2/> -->
+
+  <!-- Project 3 -->
+  <Project3/>
 </template> 
